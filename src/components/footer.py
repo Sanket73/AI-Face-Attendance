d2448038d30f3d3ca5f141ec1b399492e6214b5f
Sanket <sanketdongardive364@gmail.com>
2026-06-02 08:57:59 +0530

    streamlit setup

--- src/components/footer.py
+++ src/components/footer.py
@@ -8,3 +8,12 @@ def footer_home():
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:white;">Created wirh ❤️ by SANKET</p>
     """, unsafe_allow_html=True)
+    
+def footer_dashboard():
+
+    # logo_url = "SANKET"
+
+    st.markdown(f""" 
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;">Created wirh ❤️ by SANKET</p>
+    """, unsafe_allow_html=True)
